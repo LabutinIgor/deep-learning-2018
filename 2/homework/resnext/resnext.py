4964ad419932072cb9968cd0b73a52d4a4132d4d
LabutinIgor <Labutin.IgorL@gmail.com>
2018-10-02 19:07:56 +0300

    Made resnext Bottleneck

--- 2/homework/resnext/resnext.py
+++ 2/homework/resnext/resnext.py
@@ -1,21 +1,20 @@
 import torch.nn as nn
 from torchvision.models.resnet import ResNet
 
-__all__ = ['resnext50', 'resnext101',
-           'resnext152']
+__all__ = ['resnext50', 'resnext101', 'resnext152']
 
 
 class Bottleneck(nn.Module):
     expansion = 4
 
-    def __init__(self, inplanes, planes, stride=1, downsample=None):
+    def __init__(self, inplanes, planes, stride=1, downsample=None, cardinality=32):
         super(Bottleneck, self).__init__()
-        self.conv1 = nn.Conv2d(inplanes, planes, kernel_size=1, bias=False)
-        self.bn1 = nn.BatchNorm2d(planes)
-        self.conv2 = nn.Conv2d(planes, planes, kernel_size=3, stride=stride,
-                               padding=1, bias=False)
-        self.bn2 = nn.BatchNorm2d(planes)
-        self.conv3 = nn.Conv2d(planes, planes * 4, kernel_size=1, bias=False)
+        self.conv1 = nn.Conv2d(inplanes, planes * 2, kernel_size=1, bias=False)
+        self.bn1 = nn.BatchNorm2d(planes * 2)
+        self.conv2 = nn.Conv2d(planes * 2, planes * 2, kernel_size=3, stride=stride,
+                               padding=1, groups=cardinality, bias=False)
+        self.bn2 = nn.BatchNorm2d(planes * 2)
+        self.conv3 = nn.Conv2d(planes * 2, planes * 4, kernel_size=1, bias=False)
         self.bn3 = nn.BatchNorm2d(planes * 4)
         self.relu = nn.ReLU(inplace=True)
         self.downsample = downsample
@@ -48,7 +47,6 @@ def resnext50(pretrained_weights=None, **kwargs):
     model = ResNet(Bottleneck, [3, 4, 6, 3], **kwargs)
     if pretrained_weights is not None:
         model.load_state_dict(pretrained_weights)
-        pass
     return model
 
 
@@ -56,7 +54,6 @@ def resnext101(pretrained_weights=None, **kwargs):
     model = ResNet(Bottleneck, [3, 4, 23, 3], **kwargs)
     if pretrained_weights is not None:
         model.load_state_dict(pretrained_weights)
-        pass
     return model
 
 
@@ -64,5 +61,4 @@ def resnext152(pretrained_weights=None, **kwargs):
     model = ResNet(Bottleneck, [3, 8, 36, 3], **kwargs)
     if pretrained_weights is not None:
         model.load_state_dict(pretrained_weights)
-        pass
     return model
--- 2/homework/test/test_resnext.py
+++ 2/homework/test/test_resnext.py
@@ -4,14 +4,14 @@ import torch
 from torch.utils.data import TensorDataset
 
 
-# def test_resnext():
-#     model = resnext50(num_classes=10)
-#     trainer = Trainer(model)
-#     train_set = TensorDataset(torch.rand(5, 3, 224, 224), torch.randint(0, 10, (5,)).long())
-#     test_set = TensorDataset(torch.rand(5, 3, 224, 224), torch.randint(0, 10, (5,)).long())
-#     train_loader = torch.utils.data.DataLoader(train_set, batch_size=4)
-#     test_loader = torch.utils.data.DataLoader(test_set, batch_size=4)
-#     trainer.train(train_loader, test_loader, epoch_cnt=2)
+def test_resnext():
+    model = resnext50(num_classes=10)
+    trainer = Trainer(model)
+    train_set = TensorDataset(torch.rand(5, 3, 224, 224), torch.randint(0, 10, (5,)).long())
+    test_set = TensorDataset(torch.rand(5, 3, 224, 224), torch.randint(0, 10, (5,)).long())
+    train_loader = torch.utils.data.DataLoader(train_set, batch_size=4)
+    test_loader = torch.utils.data.DataLoader(test_set, batch_size=4)
+    trainer.train(train_loader, test_loader, epoch_cnt=2)
 
 
 def test_save_load():
